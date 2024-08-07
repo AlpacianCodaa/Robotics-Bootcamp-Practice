48c4e28b88e07b833f7d03af2e6532ebcbb79003
AlpacianCodaa <abhinav.thummalapally@gmail.com>
2024-08-07 19:59:11 -0400

    Create FourthExercise.java

---
+++ FourthExercise.java
@@ -0,0 +1,7 @@
+public class FourthExercise {
+    public static void main(String[] args){
+        // 11 feet tall
+        /* It would be more inconvenient if you were 9 inches tall as oyu wouldnt be able to do anything.
+         Atleast at 11 feet you can do something like play basketball. */
+    }
+}
