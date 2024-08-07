73de031a0c10747f13389de8116b7c0db4c634ee
AlpacianCodaa <abhinav.thummalapally@gmail.com>
2024-08-07 18:58:48 -0400

    Create FirstExercise.java

---
+++ FirstExercise.java
@@ -0,0 +1,9 @@
+public class FirstExercise {
+    public static void main(String[] args){
+        int mutableVariable = 4;
+        final String IMMUTABLE_VARIABLE = "This can't change";
+        System.out.println(mutableVariable);
+        System.out.println(IMMUTABLE_VARIABLE);
+
+    }
+}
