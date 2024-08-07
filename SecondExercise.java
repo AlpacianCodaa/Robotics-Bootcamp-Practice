2c8db0819780bd376011435d450faa82c1438f15
AlpacianCodaa <abhinav.thummalapally@gmail.com>
2024-08-07 19:20:52 -0400

    Create SecondExercise.java

---
+++ SecondExercise.java
@@ -0,0 +1,10 @@
+public class SecondExercise {
+    public static void main(String[] args){
+        int x = 4;
+        int y = 8;
+        x++;
+        y--;
+        int z =  x + y;
+        System.out.println(z);
+    }
+}
